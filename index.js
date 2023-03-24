729b4f4416f88000b56650d1576d99b918221b68
Azmin <74791015+azminzamsanirani@users.noreply.github.com>
2023-03-25 02:16:08 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const buttonElement = document.querySelector(".btn");
+
+buttonElement.addEventListener("mouseover", (event) => {
+    const x = (event.pageX - buttonElement.offsetLeft);
+    const y = (event.pageX - buttonElement.offsetTop);
+
+    buttonElement.style.setProperty("--xPos", x + "px")
+    buttonElement.style.setProperty("--yPos", y + "px")
+})
